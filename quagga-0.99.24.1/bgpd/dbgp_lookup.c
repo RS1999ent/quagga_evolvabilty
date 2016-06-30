c0cb06028a0c248581f80f556c0b8e8bed45b16f
Raja Sambasivan <rajas@ubuntu.(none)>
2016-06-30 12:13:33 -0700

    Make sure we convert to network byte order and back

--- quagga-0.99.24.1/bgpd/dbgp_lookup.c
+++ quagga-0.99.24.1/bgpd/dbgp_lookup.c
@@ -5,6 +5,8 @@
 
 #include <time.h>
 #include <stdlib.h>
+#include <netinet/in.h>
+#include <inttypes.h>
 
 #include "lib/zebra.h"
 #include "bgpd/bgpd.h"
@@ -12,7 +14,6 @@
 //#include "bgpd/bgp_route.h" 
 #include "bgpd/dbgp_lookup.h"
 #include "hiredis/hiredis.h"
-
 
 
 /* ********************* Global vars ************************** */
@@ -24,7 +25,7 @@ static int rand_init = 0;
 dbgp_result_status_t retrieve_control_info(struct attr const * const attr, dbgp_control_info_t * const control_info)
 {
   struct transit* transit;
-  unsigned int key;
+  uint32_t key;
   redisContext *c;
   redisReply* reply;
   char redis_cmd[256];
@@ -33,11 +34,12 @@ dbgp_result_status_t retrieve_control_info(struct attr const * const attr, dbgp_
    * community attributes
    */
   transit = attr->extra->transit; 
-  assert(transit->length == sizeof(int));
-  key = (int)*transit->val;
+  assert(transit->length == sizeof(uint32_t));
+  /* Convert from network to host byte order */
+  key = ntohl(*(uint32_t *)transit->val);
 
-  c = redisConnect("REDIS_IP", REDIS_PORT);
-  sprintf(redis_cmd, "GET %ud", key);
+  c = redisConnect(REDIS_IP, REDIS_PORT);
+  sprintf(redis_cmd, "GET %"PRIu32"", key);
   reply = redisCommand(c, redis_cmd); 
   assert(reply->type != REDIS_REPLY_ERROR || REDIS_REPLY_ARRAY);
   *control_info = reply->integer; 
@@ -65,7 +67,7 @@ dbgp_result_status_t set_control_info(struct attr * const attr, dbgp_control_inf
 
   /* Store control info in lookup service */
   c = redisConnect(REDIS_IP, REDIS_PORT);
-  sprintf(redis_cmd, "SET %d %lld", *key, *control_info);
+  sprintf(redis_cmd, "SET %"PRIu32" %PRIu64", *key, *control_info);
   reply = redisCommand(c, redis_cmd); 
   assert(reply->type != REDIS_REPLY_ERROR);
   free(reply); 
@@ -75,7 +77,9 @@ dbgp_result_status_t set_control_info(struct attr * const attr, dbgp_control_inf
   assert(attr->extra->transit != NULL);
   transit = attr->extra->transit;
 
-  transit->length = sizeof(int);
+  transit->length = sizeof(uint32_t);
+  /* Make sure to convert from host to network byte order*/
+  *key = htonl(*key);
   /* transit->val should be de-allocated in bgp_attr.c eventually */
   transit->val = (u_char *)key;
 						    
