e429f69b49af43540d5ab9ec602c079cd07e65ce
david <david.tranlam@gmail.com>
2017-01-25 10:26:24 -0600

    commented out the bgenchmark output filter work for cloudlabe exps

--- quagga-0.99.24.1/bgpd/benchmark.c
+++ quagga-0.99.24.1/bgpd/benchmark.c
@@ -164,47 +164,47 @@ void benchmark_update_control_info(dbgp_control_info_t* control_info) {
 dbgp_filtered_status_t benchmark_output_filter(struct attr* attr) {
   // if either the transit or extra is null, then we haven't inserted benchmark
   // information into advert (we are announcing it), therefore insert it
-  if (attr->extra == NULL || attr->extra->transit == NULL || attr->extra->transit->length == 0) {
-    if (BGP_DEBUG (update, UPDATE_IN))  
-      zlog_debug(
-                 "benchmark_output_filter: no transit or extra, means we are announcing "
-                 "it. Update control info to put benchmark info in");
-    if (BGP_DEBUG (update, UPDATE_IN))  
-      zlog_debug("benchmark_output_filter: aspath of attr: %s",
-                 attr->aspath->str);
-    uint32_t num_bytes_to_set = GetBenchmarkBytes(general_configuration_);
-    if(num_bytes_to_set > 0) {
+  /* if (attr->extra == NULL || attr->extra->transit == NULL || attr->extra->transit->length == 0) { */
+  /*   if (BGP_DEBUG (update, UPDATE_IN))   */
+  /*     zlog_debug( */
+  /*                "benchmark_output_filter: no transit or extra, means we are announcing " */
+  /*                "it. Update control info to put benchmark info in"); */
+  /*   if (BGP_DEBUG (update, UPDATE_IN))   */
+  /*     zlog_debug("benchmark_output_filter: aspath of attr: %s", */
+  /*                attr->aspath->str); */
+  /*   uint32_t num_bytes_to_set = GetBenchmarkBytes(general_configuration_); */
+  /*   if(num_bytes_to_set > 0) { */
       
-    dbgp_control_info_t* control_info;
-    struct transit* transit;
+  /*   dbgp_control_info_t* control_info; */
+  /*   struct transit* transit; */
 
-    // Precondition asserts
-    assert(attr != NULL);
-    control_info = GetControlInformation2(attr, &transit);
-    assert(transit != NULL);
-    if (BGP_DEBUG (update, UPDATE_IN))  
-      zlog_debug("benchmark_output_filter: setting %d bytes",
-                 num_bytes_to_set);
-    char* old_integrated_advertisement = control_info->integrated_advertisement;
-    int old_integrated_advertisement_size =
-        control_info->integrated_advertisement_size;
-    int new_size;
-    char* new_integrated_advertisement_info = SetBenchmarkIABytes(
-        old_integrated_advertisement, old_integrated_advertisement_size,
-        num_bytes_to_set, &new_size);
-    free(old_integrated_advertisement);
-    control_info->integrated_advertisement = new_integrated_advertisement_info;
-    control_info->integrated_advertisement_size = new_size;
-    if (BGP_DEBUG (update, UPDATE_IN))  
-      zlog_debug(
-                 "benchmark_output_filter: new advertisement: %s",
-                 SerializedAdverToString(control_info->integrated_advertisement,
-                                         control_info->integrated_advertisement_size));
-    // Insert the updated control information into the lookup service and insert
-    // the key into the the transitive attribute.
-    dbgp_result_status_t success = set_control_info(transit, control_info);
-    assert(success == DBGP_SUCCESS);
-    }
-  }
+  /*   // Precondition asserts */
+  /*   assert(attr != NULL); */
+  /*   control_info = GetControlInformation2(attr, &transit); */
+  /*   assert(transit != NULL); */
+  /*   if (BGP_DEBUG (update, UPDATE_IN))   */
+  /*     zlog_debug("benchmark_output_filter: setting %d bytes", */
+  /*                num_bytes_to_set); */
+  /*   char* old_integrated_advertisement = control_info->integrated_advertisement; */
+  /*   int old_integrated_advertisement_size = */
+  /*       control_info->integrated_advertisement_size; */
+  /*   int new_size; */
+  /*   char* new_integrated_advertisement_info = SetBenchmarkIABytes( */
+  /*       old_integrated_advertisement, old_integrated_advertisement_size, */
+  /*       num_bytes_to_set, &new_size); */
+  /*   free(old_integrated_advertisement); */
+  /*   control_info->integrated_advertisement = new_integrated_advertisement_info; */
+  /*   control_info->integrated_advertisement_size = new_size; */
+  /*   if (BGP_DEBUG (update, UPDATE_IN))   */
+  /*     zlog_debug( */
+  /*                "benchmark_output_filter: new advertisement: %s", */
+  /*                SerializedAdverToString(control_info->integrated_advertisement, */
+  /*                                        control_info->integrated_advertisement_size)); */
+  /*   // Insert the updated control information into the lookup service and insert */
+  /*   // the key into the the transitive attribute. */
+  /*   dbgp_result_status_t success = set_control_info(transit, control_info); */
+  /*   assert(success == DBGP_SUCCESS); */
+  /*   } */
+  /* } */
   return DBGP_NOT_FILTERED;
 }
